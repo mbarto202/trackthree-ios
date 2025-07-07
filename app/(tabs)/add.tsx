bef85c97640908391fbb0f820f5eddd19d8fec61
Michael <michael.d.barto@gmail.com>
2025-07-07 17:46:08 -0400

    created add screen

---
+++ app/(tabs)/add.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function AddScreen() {
+  return (
+    <View>
+      <Text style={{ color: "#fff" }}>Add Entry Manually</Text>
+    </View>
+  );
+}
