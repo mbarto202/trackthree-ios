fc8a8eac359a2e6ad7dc657bb44e5398e2986a9a
Michael <michael.d.barto@gmail.com>
2025-07-09 20:33:38 -0400

    added qr scan screen

---
+++ app/(tabs)/scan.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function ScanScreen() {
+  return (
+    <View>
+      <Text style={{ color: "#fff" }}>Scan QR Code</Text>
+    </View>
+  );
+}
