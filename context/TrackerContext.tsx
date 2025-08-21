56f61a7d0e46008b924847908b1ce4447aef7dcf
Michael <michael.d.barto@gmail.com>
2025-08-20 21:45:03 -0400

    created add functions

--- context/TrackerContext.tsx
+++ context/TrackerContext.tsx
@@ -7,6 +7,9 @@ type TrackerContextType = {
   setCalories: (value: number) => void;
   setProtein: (value: number) => void;
   setWater: (value: number) => void;
+  addCalories: (amount: number) => void;
+  addProtein: (amount: number) => void;
+  addWater: (amount: number) => void;
 };
 
 const TrackerContext = createContext<TrackerContextType | undefined>(undefined);
@@ -23,9 +26,24 @@ export const TrackerProvider = ({ children }: { children: ReactNode }) => {
   const [protein, setProtein] = useState(0);
   const [water, setWater] = useState(0);
 
+  // Add functions to increment the values
+  const addCalories = (amount: number) => setCalories((prev) => prev + amount);
+  const addProtein = (amount: number) => setProtein((prev) => prev + amount);
+  const addWater = (amount: number) => setWater((prev) => prev + amount);
+
   return (
     <TrackerContext.Provider
-      value={{ calories, protein, water, setCalories, setProtein, setWater }}
+      value={{
+        calories,
+        protein,
+        water,
+        setCalories,
+        setProtein,
+        setWater,
+        addCalories,
+        addProtein,
+        addWater,
+      }}
     >
       {children}
     </TrackerContext.Provider>
